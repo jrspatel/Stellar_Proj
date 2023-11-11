5d624e52d50a28c10912237638f65bd97241cf54
MUTHU HARINI KALIRAJ <buriburi@MUTHUs-MacBook-Pro.local>
2023-11-10 21:54:49 -0500

    airflow_setup

---
+++ dag/dags/airflow_code.py
@@ -0,0 +1,81 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+import sys
+
+import os
+
+# Add the path to the directory containing the source
+sys.path.append('/path/to/directory/containing/source')
+
+from src.datapreprocessing import split_dataset, checking_NaN, data_stats, scaling, outlier_elimination
+
+default_args = {
+    'owner': 'Team 3',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=10),
+}
+
+with DAG(
+    default_args=default_args,
+    dag_id="preprocessing_v1",
+    description="Data Preprocessing",
+    start_date=datetime(2023, 11, 8),
+    schedule_interval='@daily',
+    catchup=True,
+) as dag:
+
+    split_task = PythonOperator(
+        task_id = 'split_task',
+        python_callable = split_dataset,
+        provide_context = True,
+        # In split_task
+
+        dag = dag
+    )
+
+    check_nan_task = PythonOperator(
+    task_id='check_nan_task',
+    python_callable=checking_NaN,
+    provide_context=True,
+    dag = dag,
+)
+    
+    stats_task = PythonOperator(
+        task_id = 'stats_task',
+        python_callable = data_stats,
+        provide_context=True,
+        
+        # In split_task
+
+        dag = dag
+    )
+
+    scaling_task = PythonOperator(
+        task_id = 'scaling_task',
+        python_callable = scaling,
+        provide_context=True,
+        
+        # In split_task
+
+        dag = dag
+    )
+
+    outlier_detection_task = PythonOperator(
+        task_id = 'outlier_detection_task',
+        python_callable = outlier_elimination,
+        provide_context=True,
+        
+        # In split_task
+
+        dag = dag
+    )
+
+    #import_data_task >> split_task
+
+    split_task >> check_nan_task >> stats_task >> scaling_task >> outlier_detection_task
+    # split_task >> stats_task
+
+# If this script is run directly, allow command-line interaction with the DAG
+if __name__ == "__main__":
+    dag.cli()
---
+++ dag/dags/src/datapreprocessing.py
@@ -0,0 +1,94 @@
+"""
+Created By: Team 03
+Date: November 2, 2023
+"""
+#importing necessary Libraries
+import pandas as pd
+import numpy as np
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
+
+  
+def data_stats():
+    dataset = pd.read_csv(os.path.join(os.path.dirname(__file__), "../data/star_classification.csv"))
+    variable_stats = dataset.describe()
+    return variable_stats
+
+def split_dataset():
+
+    dataset = pd.read_csv(os.path.join(os.path.dirname(__file__), "../data/star_classification.csv"))
+    y = dataset['class']
+    X = dataset.drop(columns = ['class'], axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, stratify = y, random_state=0)
+    X_train['class'] = y_train.to_list()
+    X_test['class'] = y_test.to_list()
+    
+    X_train.to_csv(os.path.join(os.path.dirname(__file__), "../data/X_train.csv"), index=False)
+    
+    X_test.to_csv(os.path.join(os.path.dirname(__file__), "../data/X_test.csv"), index=False)
+    path = "dags/data/star_classification.csv"
+    return 1
+
+def checking_NaN():
+    dataset = pd.read_csv(os.path.join(os.path.dirname(__file__), "../data/X_train.csv"))
+    nan_rows = len(dataset[dataset.isna().any(axis=1)])
+    
+    if nan_rows != 0:
+        nan_index = dataset[dataset.isna().any(axis=1)].index[0]
+        dataset = dataset.drop(labels = nan_index, axis=0)
+        dataset = dataset.reset_index(drop = True)
+    
+    dataset.to_csv(os.path.join(os.path.dirname(__file__), "../data/X_train_nan.csv"), index=False)
+    return 1
+
+def scaling():
+    dataset = pd.read_csv(os.path.join(os.path.dirname(__file__), "../data/X_train_nan.csv"))
+    scaling = MinMaxScaler()
+    y = dataset['class']
+    X_train = dataset.drop(['class'], axis=1)
+    X_train_columns = X_train.columns
+    X_train_array = scaling.fit_transform(X = X_train)
+    X_train = pd.DataFrame(X_train_array)
+    X_train.columns = X_train_columns
+    X_train['class'] = y
+    
+    X_train.to_csv(os.path.join(os.path.dirname(__file__), "../data/X_train_scaling.csv"), index=False)
+    return 1
+
+def outlier_elimination():
+    # Assuming dataset is a pandas DataFrame
+    dataset = pd.read_csv(os.path.join(os.path.dirname(__file__), "../data/X_train_scaling.csv"))
+    y = dataset['class']
+    X_train = dataset.drop(['class'], axis=1)
+    Q1 = X_train.quantile(0.25)
+    Q3 = X_train.quantile(0.75)
+    IQR = Q3 - Q1
+
+    # Define a threshold to identify outliers
+    threshold = 1.0
+
+    # Identify outliers
+    outliers = ((X_train < (Q1 - threshold * IQR)) | (X_train > (Q3 + threshold * IQR))).any(axis=1)
+
+    # Remove outliers
+    dataset_no_outliers = X_train[~outliers]
+    dataset_no_outliers['class'] = y
+    
+    dataset_no_outliers.to_csv(os.path.join(os.path.dirname(__file__), "../data/X_train_outlier.csv"), index=False)
+    return 1
+
+def pcadatset(X_t):
+    """
+        @Parameters:
+            X_t(DataFrame): Train dataset
+        @does:
+            Perform PCa
+        @Returns:
+            X_train(Dataset): Dataset after removing few columns
+    """
+    principal = PCA(n_components=3)
+    principal.fit(X_t)
+    x = principal.transform(X_t)
+    return x
